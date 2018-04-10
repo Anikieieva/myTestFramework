47340ea96d82f37002af35466433181a4da16445
Оксана <myletters.oxan@gmail.com>
2018-04-10 15:31:17 +0300

    create test class

---
+++ HotLineTests/HotLineTests/HotlineHomepageTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HotLineTests
+{
+    [TestFixture]
+    public class HotlineHomepageTest
+    {
+        [OneTimeSetUp] // вызывается перед началом запуска всех тестов
+        public void OneTimeSetUp()
+        {
+            // ТУТ КОД
+        }
+
+        [OneTimeTearDown] //вызывается после завершения всех тестов
+        public void OneTimeTearDown()
+        {
+            // ТУТ КОД
+        }
+
+        [SetUp] // вызывается перед каждым тестом
+        public void SetUp()
+        {
+            // ТУТ КОД
+        }
+
+        [TearDown] // вызывается после каждого теста
+        public void TearDown()
+        {
+            // ТУТ КОД
+        }
+
+        [Test]
+        public void TEST_1()
+        {
+            // ТУТ КОД
+        }
+
+        [Test]
+        public void TEST_2()
+        {
+            // ТУТ КОД
+        }
+    }
+}
